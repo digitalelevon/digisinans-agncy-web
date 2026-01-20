5ed9007aa4f1aefe516c7613e7eff95051bdbe4e
digitalelevon <sinanmc46@gmail.com>
2026-01-20 21:12:00 +0530

    Fix: restore Next.js sitemap.ts + resolve GSC sitemap issue

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,37 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: {
-            userAgent: '*',
-            allow: '/',
-        },
+        rules: [
+            {
+                userAgent: 'Googlebot',
+                allow: '/',
+            },
+            {
+                userAgent: 'googlebot-image',
+                allow: '/',
+            },
+            {
+                userAgent: 'googlebot-mobile',
+                allow: '/',
+            },
+            {
+                userAgent: 'MSNBot',
+                allow: '/',
+            },
+            {
+                userAgent: 'Slurp',
+                allow: '/',
+            },
+            {
+                userAgent: 'yahoo-mmcrawler',
+                allow: '/',
+            },
+            {
+                userAgent: '*',
+                allow: '/',
+                disallow: '/cgi-bin/',
+            },
+        ],
         sitemap: 'https://digisinans.in/sitemap.xml',
     }
 }
